14606679bb7d71f02c8f08dbaaacf02df75c85c4
jawed <jawed@LAPTOP-BTOBCIRH>
2025-10-03 21:05:53 +0530

    Day 8 successfully committed

---
+++ Core_Java/src/day7/overriding/SBI.java
@@ -0,0 +1,13 @@
+package day7.overriding;
+
+public class SBI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 7.0f;
+	}
+
+	public SBI getObject() {
+		return this;
+	}
+
+}
