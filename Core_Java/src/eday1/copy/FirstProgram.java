a1c52f368a36304e8ee36983b3db6b57f78ff6d7
jawed <jawed@LAPTOP-BTOBCIRH>
2025-10-06 11:10:04 +0530

    name change

---
+++ Core_Java/src/eday1/copy/FirstProgram.java
@@ -0,0 +1,7 @@
+package eday1.copy;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java...");
+	}
+}
---
+++ Core_Java/src/eday2/copy/Whiledemo.java
@@ -0,0 +1,14 @@
+package eday2.copy;
+
+public class Whiledemo {
+	public static void main(String[] args) 
+	{
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
