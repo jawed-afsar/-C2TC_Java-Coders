0e33a2b74eed0be046a45c14e0f2e5f1c525fe62
jawed <jawed@LAPTOP-BTOBCIRH>
2025-10-24 20:59:43 +0530

    Day 9,10,11,12,13

---
+++ Core_Java/src/com/JawedAfsarJ/tnsif/day12/InvalidMarksException.java
@@ -0,0 +1,13 @@
+package com.JawedAfsarJ.tnsif.day12;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+}
---
+++ Core_Java/src/com/JawedAfsarJ/tnsif/day9/StringSubclass.java
@@ -0,0 +1,10 @@
+package com.JawedAfsarJ.tnsif.day9;
+
+public class StringSubclass extends String {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
